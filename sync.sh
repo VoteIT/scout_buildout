5fa194a1f539573f27a804cccd62038d556e6c60
Robin Harms Oredsson <robin@betahaus.net>
2018-11-06 12:19:17 +0100

    Server conf for live meeting

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -Pz voteit@scout.voteit.se:~/scout_buildout/var/Data.fs var/Data.fs
-rsync -Prz voteit@scout.voteit.se:~/scout_buildout/var/blob var/
-rsync -Prz voteit@scout.voteit.se:~/scout_buildout/var/log var/.
+rsync -Pz voteit@scout.voteit.se:~/srv/scout_buildout/var/Data.fs var/Data.fs
+rsync -Prz voteit@scout.voteit.se:~/srv/scout_buildout/var/blob var/
+rsync -Prz voteit@scout.voteit.se:~/srv/scout_buildout/var/log var/.
